bb2a3eaae765a7aa25c45602b0d25aebe6df2e7e
Adrian Toma <atoma@bitdenfender.com>
2014-07-18 18:24:31 +0300

    small fixes
    Moved example to example/
    removed some console.log

--- example/get_leagues.js
+++ example/get_leagues.js
@@ -1,6 +1,6 @@
 
 var Dota2Api = require('../lib/dota2api.js');
-var dota     = new Dota2Api('830F9D2CE8476B24E02F386E2694972C');
+var dota     = new Dota2Api('<key>');
 
 dota.GetLeagues( function ( err , data ) 
 {
